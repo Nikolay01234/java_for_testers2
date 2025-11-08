e52c2deb23b2b4dd7641e677cc5bcb3e61e5b0aa
Nikolay01234 <prigorodovnikolay@yandex.ru>
2025-11-08 22:45:32 +0400

    Лекция 3.3. первая часть
    Реализована двухуровневая архитектура, но классы ещё не разложены по пакетам

--- addressbook_web_tests/src/test/java/GroupRemovalTests.java
+++ addressbook_web_tests/src/test/java/GroupRemovalTests.java
@@ -5,11 +5,16 @@ public class GroupRemovalTests extends TestBase {
 
     @Test
     public void CanRemoveGroup() {
-        openGroupsPage();
-        if (!isGroupPresent()) {
-            createGroup(new GroupData("", "", ""));
+        // код, который незакомментирован на строках 9, 10, 13 совпадает с кодом показанным в Лекции 3.3. таймкод - 4:12
+        app.openGroupsPage();
+        if (!app.isGroupPresent()) {
+            //app.createGroup(new GroupData("", "", ""));
+            // добавил вариант ниже,т.к. он ближе к тому,что показано на на -5:00 Лекция 3.3
+            app.createGroup(new GroupData("group name", "group header", "group footer"));
         }
-        removeGroup();
+//        if (!isGroupPresent()) { // на -5:00 Лекция 3.3 показан именно такой вид
+//            createGroup(new GroupData("group name", "group header", "group footer"));
+//        }
+        app.removeGroup();
     }
-
 }
